052da1da39ceb43481329a7ca99c227416242559
Devanshi Prasad <102360503+devanshiprasad@users.noreply.github.com>
2023-06-15 22:47:48 +0530

    Add files via upload
    src folder

---
+++ src/scenes/Landing.jsx
@@ -0,0 +1,111 @@
+import SocialMediaIcons from "../components/SocialMediaIcons";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const Landing = ({ setSelectedPage }) => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
+    >
+      {/* IMAGE SECTION */}
+      <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
+        {isAboveLarge ? (
+          <div
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rectangle-t-[400px]
+            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-purple before:z-[-1]"
+          >
+            <img
+              alt="profile"
+              className="hover:filter hover:saturate-200 transition duration-500 z-[10] w-full max-w-[200px] md:max-w-[400px]"
+              src={require('./devanshi.jpg')}
+            />
+          </div>
+        ) : (
+          <img
+            alt="profile"
+            className="z-[10] w-full max-w-[200px] md:max-w-[400px]"
+            src="./devanshi.jpg"
+          />
+        )}
+      </div>
+
+      {/* MAIN TEXT */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-playfair z-{10} text-center md:text-start">
+            Devanshi {""}
+            <span
+              className="xs:relative xs:text-deep-blue xs:font-semibold z-{-2} xs:before:content-brush 
+              before:absolute before:-left-[250px] before:-top-[150px] before:z-[-3] max-w-[200px] md:max-w-[400px] "
+              
+            >
+              Prasad
+            </span>
+          </p>
+
+          <p className="mt-10 mb-7 text-md text-center md:text-start xs:text-purple">
+            <b>Hey! I'm an aspiring CSE student who is passionate about bringing her creative ideas to life through coding. I'm a firm believer in a project-based learning approach, which means that I love to apply what I've learned to real-world projects as soon as possible.</b>
+          </p>
+        </motion.div>
+
+        {/* CALL TO ACTIONS */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className="bg-gradient-rainpurple text-deep-blue rounded-sm py-3 px-7 font-semibold
+              hover:bg-blue hover:text-purple transition duration-500"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            Contact Me
+          </AnchorLink>
+          
+            <section className="rounded-r-sm bg-gradient-rainpurple py-0.5 pr-0.5">
+            <div className="bg-deep-blue hover:text-yellow transition duration-500 w-full h-full flex items-center justify-center px-10 font-playfair">
+             <a href="https://www.linkedin.com/in/devanshi-prasad/" target="blank"> Resume</a>
+            </div>
+            </section>
+  
+        </motion.div>
+
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,141 @@
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+const MySkills = () => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      {/* HEADER AND IMAGE SECTION */}
+      <div className="md:flex md:justify-between md:gap-16 mt-32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            MY <span className="text-purple">SKILLS</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+            Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
+            fames odio in at.
+          </p>
+        </motion.div>
+        
+        <div className="mt-16 md:mt-0">
+          {isAboveLarge ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+              before:w-full before:h-full before:border-2 before:border- before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                className="z-10"
+                src={require("./banner.png")}
+              />
+            </div>
+          ) : (
+            <img alt="skills" className="z-10" src="./banner.png" />
+          )}
+        </div>
+      </div>
+
+      {/* SKILLS */}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* EXPERIENCE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">01</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Languages/Frameworks
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-darkpurple absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            1.Java <br/> 2.Javascript <br/>3.ReactJS <br/> 4.Python <br/>5.MySQL
+          </p>
+        </motion.div>
+
+        {/* INNOVATIVE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">02</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Certifications
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-darkpurple absolute right-0 top-0 z-[-1]"
+             img src='./' />
+          </div>
+          <p className="mt-5">
+           1. <a className="azure"href="https://www.credly.com/badges/aa32367f-d172-4adc-9249-097f6174a108/linked_in_profile" target="blank">Microsoft Certified: Azure Fundamentals</a> <br/>
+           2. <a className="ux" href="https://www.coursera.org/account/accomplishments/verify/MJEXBUCX8JB6" target="blank"> Foundations of User Experience (UX) Design (Google)</a> <br/>
+           3. <a className="design" href="https://www.freecodecamp.org/certification/devanshi_yuh/responsive-web-design"> Responsive Web Design Certification</a>
+          </p>
+        </motion.div>
+        {/* IMAGINATIVE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">03</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Additional Skills
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-darkpurple absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+          1. Figma <br/>
+          2. AWS <br/>
+          3. Wireframing <br/>
+          4. User Research <br/>
+          5. Linux 
+
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default MySkills;
